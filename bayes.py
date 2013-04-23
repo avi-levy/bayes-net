db11feaab22c60bb87183ec17238628530968a1e
avi_l <avi142@utdallas.edu>
2013-04-23 00:04:27 -0500

    some refactored stuff

--- bayes.py
+++ bayes.py
@@ -2,48 +2,6 @@
 
 from sys import argv
 
-events = []
-
-class event(object):
-        def __init__(self, tableHeading):
-                '''
-                tableHeading is the heading of the data table for this event
-                A B | C
-                this means we are event C and we are populating with 4 (A,B) values
-                '''
-                self.parents = []
-                self.name = None
-                self.data = {}                
-                if tableHeading.startswith('P('):# P(A) = val
-                        left, right = tuple(tableHeading.split("="))
-                        self.name = left.split("(")[1].split(")")[0]
-                        self.data = float(right)
-                        #print "Created event named %s with data %f and no parents" % (self.name, self.data)
-                        return
-                parents, name = tuple(tableHeading.split('|')) # there can be a parse error here
-                self.parents = parents.split()# important: order is preserved
-                self.name = name
-                #print "Create event named %s and parents %s" % (self.name, self.parents)
-        def setProbabilities(self, row):
-                '''
-                ignore row if startswith -
-                row is a string. we want to convert it to a dictionary of eventnames to truth values:
-                {'A' : True, 'B' : False, ... } and then a value
-                
-                TODO: add consistency checks against the parents we intialized with
-                '''
-                if row.startswith('-'):
-                        return
-                #print "called with %s" % row
-                bools, value = tuple(row.split("|"))
-                #print "split %s" % bools.split()
-                #print "len %d" % len(tuple(bools.split()))
-                self.data[tuple(bools.split())] = float(value)
-                #print "Data for event %s is now %s" % (self.name, self.data)
-        def __repr__(self):
-                return "Event %s:\nParents: %s\nData:\n%s" % (self.name, self.parents, self.data)
-        def __str__(self):
-                return self.__repr__()
 usage = "usage: bayes <bayesnet> <enum|elim> <query>\n<bayesnet> is the name of the file containing the bayes net.\n<enum|elim> specify the algorithm to use.\n<query> is the probability to compute, specified in quotes."
 def end(message=usage):
   print message
@@ -63,7 +21,7 @@ try:
         else:
                 query = query[0]
         first, last = tuple(query.split("|"))
-        requestedEvent = first.split("(")[1]
+        var = first.split("(")[1]
         givens = {}
         for term in last.strip(")").split(","):
                 name, value = tuple(term.split("="))
@@ -71,23 +29,4 @@ try:
 except Exception:
         end('Could not parse query string. Make sure it is of this form, INCLUDING quotes: "P(C|A=f,E=t)"')
         
-#print requestedEvent        
-#print givens
-
-cur = None
-with open(infile) as infile:
-  for line in infile:
-    line = line.strip()
-    if line:
-      # print "parsing line %s" % line
-      if cur:
-        cur.setProbabilities(line)
-      else:
-        cur = event(line)
-    else:
-      events.append(cur)
-      cur = None
-if cur:
-        events.append(cur)
-
-# events is a list of bayes net data. requestedEvent is the event we are trying to compute; givens is the condition under which we compute it
+bn = net(infile)
---
+++ net.py
@@ -0,0 +1,62 @@
+class net(object):
+        def __init__(self, file):
+                self.entries = []
+                self.vars = set()
+                entry = None
+                for line in open(file):
+                        line = line.strip()
+                        if line:
+                                if entry:
+                                        entry.setProbabilities(line)
+                                else:
+                                        entry = event(line)
+                        else:
+                                self.add(entry)
+                                entry = None
+                if entry:
+                        events.append(entry)                
+
+        def add(self, entry):
+                self.entries.append(entry)
+                self.vars.add(entry.name)
+        def __repr__(self):
+                return "Bayes Net with vars %s and entries:\n%s" % (self.vars, self.entries)
+                
+class event(object):
+        def __init__(self, tableHeading):
+                '''
+                tableHeading is the heading of the data table for this event
+                A B | C
+                this means we are event C and we are populating with 4 (A,B) values
+                '''
+                self.parents = []
+                self.name = None
+                self.data = {}                
+                if tableHeading.startswith('P('):# P(A) = val
+                        left, right = tuple(tableHeading.split("="))
+                        self.name = left.split("(")[1].split(")")[0]
+                        self.data = float(right)
+                        #print "Created event named %s with data %f and no parents" % (self.name, self.data)
+                        return
+                parents, name = tuple(tableHeading.split('|')) # there can be a parse error here
+                self.parents = parents.split()# important: order is preserved
+                self.name = name
+                #print "Create event named %s and parents %s" % (self.name, self.parents)
+        def setProbabilities(self, row):
+                '''
+                ignore row if startswith -
+                row is a string. we want to convert it to a dictionary of eventnames to truth values:
+                {'A' : True, 'B' : False, ... } and then a value
+                
+                TODO: add consistency checks against the parents we intialized with
+                '''
+                if row.startswith('-'):
+                        return
+                #print "called with %s" % row
+                bools, value = tuple(row.split("|"))
+                #print "split %s" % bools.split()
+                #print "len %d" % len(tuple(bools.split()))
+                self.data[tuple(bools.split())] = float(value)
+                #print "Data for event %s is now %s" % (self.name, self.data)
+        def __repr__(self):
+                return "Event %s:\nParents: %s\nData:\n%s" % (self.name, self.parents, self.data)
